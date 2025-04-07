c1e4f881dd6eb4de89af988a913ca407395fc6fb
Liweiwu6 <53037825+liweiwu6@users.noreply.github.com>
2025-04-07 12:00:20 +0800

    位于分支 main 您的分支与上游分支 'origin/main' 一致。
    要提交的变更：
    	修改：     src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
   ros::NodeHandle nh;
   image_transport::ImageTransport it(nh);
   LIVMapper mapper(nh);
-  mapper.initializeSubscribersAndPublishers(nh, it);
+  mapper.initializeSubscribersAndPublishers(nh, it);//初始化订阅器和发布器
   mapper.run();
   return 0;
 }
